3a8ba30d9858f4e4f757ea8bf731c66092c64cda
rvallen-flyte <rvallen@ucsc.edu>
2025-10-27 14:17:27 -0700

    labs
    added labs

---
+++ excercises/lab4.js
@@ -0,0 +1,26 @@
+let myCommutes = ["UCSC bus", "E-Bike", "Car", "Walking"];
+//array
+
+let myFavouriteCommute = {
+    type: "Bike",
+    tires: "2",
+    color: "black",
+    hasMiddleDoor: true,
+    speed: ["twenty mph", "ten mph"],
+
+};
+
+//text variable, number variable, text var 2, boolean, array
+
+let megaSentence;   
+
+megaSentence = "<p> My top two paths to campus are:" + myWays[0] + ", " + myWays[3] + "</p>";
+
+megaSentence = megaSentence + "<p>My favourite commute possesses such characteristics: type - " 
++ myFavouriteCommute.type + ", color - " + myFavouriteCommute.color + 
+", a max speed of: " + myFavouriteCommute.speed[0] + "</p>";
+//whoooooole bunch of appending. so much appending. wait this is just python again ugh i hate python
+//oh shit it says to change your examples ugh
+
+$("#output").html(megaSentence);
+//allows output
